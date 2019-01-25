5f6d841c005d9569a876c78bc5ce28faa6ea8757
PPitson <pitu5599@gmail.com>
2019-01-25 16:10:57 +0100

    Extract prices from text returned by ocr and present results in pretty format

--- demo.py
+++ demo.py
@@ -1,10 +1,20 @@
-from scanner import scan_image
+import re
 
 from prices_extractor import extract_prices_column, recognize_column
+from scanner import scan_image
 
-path = "Paragony-test/IMG_20181112_194547.jpg"
+PATH = "Paragony-test/IMG_20181112_194547.jpg"
+PRICES_PATTERN = re.compile(r"(?<==)\s([\d\.\,]+)")
 
-scanned_image = scan_image(path, show_steps=False)
+
+def get_price(price_text: str) -> float:
+    price_match = re.search(PRICES_PATTERN, price_text)
+    if price_match is None:
+        return 0.0
+    return float(price_match.group().replace(",", "."))
+
+
+scanned_image = scan_image(PATH, show_steps=False)
 
 # selected manaully by some kind of gui
 prices_names_region = (720, 2400)
@@ -12,8 +22,15 @@ prices_image, names_image = extract_prices_column(scanned_image, prices_names_re
 prices_text = recognize_column(prices_image)
 names_text = recognize_column(names_image)
 
-# todo
-# prepare some simple gui to select prices_names_region (described in `def prices_names_region`)
-# extract prices from prices_text. Sth like pattern `\d+,\d\d`  that is present after `=` sign
-# match prices with names
-# display prices and names as table
+prices = [price_text for price_text in prices_text.split("\n") if price_text][1:]  # [1:] to remove noisy header
+products = [product for product in names_text.split("\n") if product][1:]
+
+width = 30
+total_sum = 0
+for product_name, price_text in zip(products, prices):
+    price = get_price(price_text)
+    print(f"{product_name:{width}}{price}")
+    # print(product_name, price)
+    total_sum += price
+
+print("Sum:", round(total_sum, 2))
